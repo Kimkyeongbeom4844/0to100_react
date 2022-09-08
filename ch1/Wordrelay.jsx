2dfd4ce72f2ea1bcf7dfa21dc8841dc5aaa04c64
김경범 <xops09685@gmail.com>
2022-09-08 23:19:13 +0900

    함수컴포넌트-끝말잇기 웹팩빌드

--- ch1/Wordrelay.jsx
+++ ch1/Wordrelay.jsx
@@ -1,10 +1,44 @@
 const React = require("react");
-const { useState } = require("react");
+const { useState, useRef } = require("react");
 
 const Wordrelay = () => {
-  const [first, setFirst] = useState("hello Webpack");
+  const [first, setFirst] = useState("제로초");
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState("");
+  const inputFocus = useRef(null);
 
-  return <h1>{first}</h1>;
+  const changeInput = (e) => {
+    setInputValue(e.currentTarget.value);
+  };
+  const checkResult = (e) => {
+    e.preventDefault();
+    if (first[first.length - 1] === inputValue[0]) {
+      setFirst(inputValue);
+      setResult("딩동댕");
+    } else {
+      setResult("땡");
+    }
+    setInputValue("");
+    inputFocus.current.focus();
+  };
+
+  return (
+    <>
+      <h2>{first}</h2>
+      <form onSubmit={checkResult}>
+        <input
+          ref={inputFocus}
+          type="text"
+          name=""
+          id=""
+          onChange={changeInput}
+          value={inputValue}
+        />
+        <button onClick={checkResult}>입력!</button>
+      </form>
+      <h2>{result}</h2>
+    </>
+  );
 };
 
 module.exports = Wordrelay;
